621a6e3c1f45de2e7c2c1fd4a6e23747f332f908
mrchar <mrchar@qq.com>
2022-12-08 23:09:24 +0800

    初始化Capacitor

---
+++ web/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.github.mrchar.zzplant',
+  appName: '发财树',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
