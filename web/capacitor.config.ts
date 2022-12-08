a05da2521194b1699dfd2e26b9fd109435f5a917
mrchar <mrchar@qq.com>
2022-12-08 23:18:46 +0800

    添加安卓和IOS

--- web/capacitor.config.ts
+++ web/capacitor.config.ts
@@ -1,10 +1,16 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import {CapacitorConfig} from "@capacitor/cli"
 
 const config: CapacitorConfig = {
-  appId: 'com.github.mrchar.zzplant',
-  appName: '发财树',
-  webDir: 'dist',
-  bundledWebRuntime: false
-};
+    appId: "com.github.mrchar.zzplant",
+    appName: "发财树",
+    webDir: "dist",
+    bundledWebRuntime: false,
+    android: {
+        path: "../android",
+    },
+    ios: {
+        path: "../ios",
+    },
+}
 
-export default config;
+export default config
