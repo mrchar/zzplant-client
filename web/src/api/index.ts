485f85cce1c27717a9d475344c5873402728ae1d
mrchar <mrchar@qq.com>
2023-04-08 14:45:15 +0800

    使用Header传递SessionId

--- web/src/api/index.ts
+++ web/src/api/index.ts
@@ -2,12 +2,24 @@ import axios from "axios"
 import auth from "./auth"
 import shop from "./shop"
 
+const sessionIdHeaderKey = "X-Auth-Token"
+
 axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL
-axios.defaults.withCredentials = true
-axios.defaults.xsrfCookieName = "XSRF-TOKEN"
-axios.defaults.xsrfHeaderName = "X-XSRF-TOKEN"
-axios.interceptors.response.use(
-    (res) => {
+
+axios.interceptors.request.use((config) => {
+    const sessionId = window.localStorage.getItem(sessionIdHeaderKey)
+    if (sessionId) {
+        config.headers.set(sessionIdHeaderKey, sessionId)
+    }
+    return config
+})
+
+axios.interceptors.response.use((res) => {
+        // @ts-ignore
+        if (res.headers.has(sessionIdHeaderKey)) {
+            // @ts-ignore
+            window.localStorage.setItem(sessionIdHeaderKey, res.headers.get(sessionIdHeaderKey))
+        }
         return res.data
     },
 )
