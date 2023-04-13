185426ef68ba5a26e36780d4b085cba30a7b2bb4
mrchar <mrchar@qq.com>
2023-04-13 23:16:59 +0800

    添加商品使用input-number

--- web/src/views/AddCommodity.vue
+++ web/src/views/AddCommodity.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import {computed, ref} from "vue"
-import api, {AddCommodityParams} from "../api"
+import api from "../api"
 import {useShop} from "../store/shop"
 import {Shop} from "../types"
 import {useRouter} from "vue-router"
@@ -18,10 +18,22 @@ const shop = computed((): Shop => {
     return {address: "", code: "", company: "", name: "", owner: ""}
 })
 
-const formData = ref<AddCommodityParams>({name: "", price: 0})
+const name = ref("")
+const price = ref<number | null>(0)
+
+function onBalanceInputBlur() {
+    if (price.value === null) {
+        price.value = 0
+    }
+}
 
 const addCommodity = () => {
-    api.shop.addCommodity(shop.value.code, formData.value)
+    if (price.value === null) {
+        ElMessage.warning("请输入价格")
+        return
+    }
+
+    api.shop.addCommodity(shop.value.code, {name: name.value, price: price.value})
         .then(() => {
             router.push("/shops/detail")
         })
@@ -36,14 +48,17 @@ const addCommodity = () => {
 <template>
     <div class="max-w-lg w-full h-full mx-auto flex flex-col gap-4">
         <zz-title title="添加商品"/>
-        <el-form label-position="top">
+        <el-form size="large" label-position="top">
             <el-form-item label="名称">
-                <el-input v-model="formData.name"/>
+                <el-input v-model="name"/>
             </el-form-item>
             <el-form-item label="价格">
-                <el-input v-model.number="formData.price">
-                    <template #prefix>￥</template>
-                </el-input>
+                <el-input-number v-model.number="price"
+                                 :max="9999999" :min="0"
+                                 :precision="2"
+                                 :value-on-clear="0"
+                                 @focus="price=null"
+                                 @blur="onBalanceInputBlur()"/>
             </el-form-item>
             <el-form-item>
                 <el-button class="w-full" type="primary" @click="addCommodity">
